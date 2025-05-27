2b929fc585237cc004f2d795fde44bce11a66a91
MaruSmall <marina_gritsanik@mail.ru>
2025-05-27 12:21:04 +0300

    добавлена инициализация первых пользователей и их ролей

---
+++ Mango.Services.IdentityServices/Initializer/DbInitializer.cs
@@ -0,0 +1,65 @@
+﻿using Duende.IdentityModel;
+using Mango.Services.IdentityServices.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Mango.Services.IdentityServices.Initializer
+{
+    public static class DbInitializer 
+    {
+        public static void Initialize(UserManager<ApplicationUser> _userManager, RoleManager<IdentityRole> _roleManager) 
+        {
+            if (_roleManager.FindByNameAsync(Consts.Admin).Result == null)
+            {
+                _roleManager.CreateAsync(new IdentityRole(Consts.Admin)).GetAwaiter().GetResult();
+            }
+            else { return; }
+
+            ApplicationUser adminUser = new ApplicationUser()
+            {
+                UserName = "admin1@gmail.com",
+                Email = "admin1@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "11111111111",
+                FirstName = "Ben",
+                LastName ="Admin"
+                
+            };
+
+            _userManager.CreateAsync(adminUser, "Admin123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(adminUser, Consts.Admin).GetAwaiter().GetResult();
+
+            var temp1 = _userManager.AddClaimsAsync(adminUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, adminUser.FirstName + " "+ adminUser.LastName),
+                new Claim(JwtClaimTypes.GivenName, adminUser.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, adminUser.LastName),
+                new Claim(JwtClaimTypes.Role, Consts.Admin),
+
+            }).Result;
+
+            ApplicationUser customerUser = new ApplicationUser()
+            {
+                UserName = "customer1@gmail.com",
+                Email = "customer@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "11111111111",
+                FirstName = "Ben",
+                LastName = "customer"
+
+            };
+
+            _userManager.CreateAsync(customerUser, "Admin123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(customerUser, Consts.Customer).GetAwaiter().GetResult();
+
+            var temp2 = _userManager.AddClaimsAsync(customerUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, customerUser.FirstName + " "+ customerUser.LastName),
+                new Claim(JwtClaimTypes.GivenName, customerUser.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, customerUser.LastName),
+                new Claim(JwtClaimTypes.Role, Consts.Customer),
+
+            }).Result;
+        }
+    }
+}
--- Mango.Services.IdentityServices/Migrations/20250520085040_AddFirstLastNameToUser.Designer.cs
+++ Mango.Services.IdentityServices/Migrations/20250520085040_AddFirstLastNameToUser.Designer.cs
@@ -4,6 +4,7 @@ using Mango.Services.IdentityServices.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.IdentityServices.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250520085040_AddFirstLastNameToUser")]
+    partial class AddFirstLastNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
